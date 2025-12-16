6ccef21784a7cce3626b75f3a2d5dce9cd42eb3c
Can <Opradyus@outlook.com>
2025-12-16 23:13:29 +0300

    style: kod düzenlemeleri yapıldı ve PrismaClient yapılandırması güncellendi

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,12 @@
 // src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"], // İstersen sorguları terminalde görmek için bunu açık tutabilirsin
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
